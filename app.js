053b62f5ee405af2cbebe99114067264917134c6
Ade Akbar Magri Dinata <160657955+adeak-bar25@users.noreply.github.com>
2025-05-17 23:55:38 +0800

    Menambahkan hamburger menu untuk tiap halaman, memindahkan nav dari file template ke js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
 const cookieParser = require('cookie-parser');
+const { error } = require('console');
 
 const port = process.env.PORT || 3000;
 
@@ -13,19 +14,17 @@ app.use(cookieParser());
 const jsonFilePath = './data/database.json';
 const jsonDB = JSON.parse(fs.readFileSync(jsonFilePath));
 
-app.get('/', (req, res) => res.render('index'));
+app.get('/', (req, res) => generate.webPage(res ,'index'));
 
-app.get('/new', (req, res) => {
-    res.render('new')
-});
+app.get('/new', (req, res) => generate.webPage(res ,'new'));
 
-app.get('/help',(req, res) => res.render('help'));
+app.get('/help',(req, res) => generate.webPage(res ,'help'));
 
-app.get('/join',(req, res) => res.render('join'));
+app.get('/join',(req, res) => generate.webPage(res ,'join'));
 
-app.get('/login', (req, res) => res.render('login'))
+app.get('/login', (req, res) => generate.webPage(res ,'login'));
 
-app.get('/thanks', (req, res) => res.render('thanks'))
+app.get('/thanks', (req, res) => generate.webPage(res ,'thanks'))
 
 app.get('/dashboard', (req, res) => {
     const i = getEvent.index(parseInt(req.query.code));
@@ -40,7 +39,8 @@ app.get('/dashboard', (req, res) => {
         let f = generate.eventFeedbackHtml(feedback.feedback, feedback.name)
         fArray.push(f)
     });
-    res.render('dashboard', {code: req.query.code, eventName: jsonDB.events[getEvent.index(req.query.code)].eventName, fblength: jsonDB.events[i].feedback.length , feedback: fArray.join(' ')});
+    // res.render('dashboard', {code: req.query.code, eventName: jsonDB.events[getEvent.index(req.query.code)].eventName, fblength: jsonDB.events[i].feedback.length , feedback: fArray.join(' ')});
+    generate.webPage(res ,'dashboard', {code: req.query.code, eventName: jsonDB.events[getEvent.index(req.query.code)].eventName, fblength: jsonDB.events[i].feedback.length , feedback: fArray.join(' ')});
 });
 
 app.post('/newsession', (req, res) => {
@@ -58,12 +58,12 @@ app.post('/newsession', (req, res) => {
 
 app.post('/join', (req, res) => {
     const code = req.body.code;
-    res.redirect('/feedback?code=' + code);
+    res.redirect(`/feedback?code=${code}`);
 })
 
 app.post('/login', (req, res) => {
     const code = parseInt(req.body.code)
-    if(!getEvent.availableCode().includes(code)) return res.render('login', {error: generate.errorHtml('Kode yang anda masukkan tidak ditemukan!')});
+    if(!getEvent.availableCode().includes(code)) return generate.webPage(res ,'login', {error: generate.errorHtml('Kode yang anda masukkan tidak ditemukan!')});
     bcrypt.compare(req.body.password, getEvent.passwordHash(code), (err, result) => {
         if(err) console.log(err);
         if(result) {
@@ -77,27 +77,31 @@ app.post('/login', (req, res) => {
             res.cookie('access', accessCode, { maxAge: 86400000, httpOnly: true });
             res.redirect('/dashboard?code=' + code);
         } else {
-            res.render('login', {error: generate.errorHtml('Password yang anda masukkan salah!')});
+            // res.render('login', {error: generate.errorHtml('Password yang anda masukkan salah!')});
+            generate.webPage(res ,login, {error: generate.errorHtml('Password yang anda masukkan salah!')});
         }
     });
 });
 
 app.get('/feedback',(req, res) => {
     if(req.query.code === NaN || req.query.code === undefined){
-        return res.render('join', {error: generate.errorHtml("Masukkan Code terlebih dahulu")});
+        // return res.render('join', {error: generate.errorHtml("Masukkan Code terlebih dahulu")});
+        return generate.webPage(res ,'join', {error: generate.errorHtml("Masukkan Code terlebih dahulu")})
     }else if(!getEvent.availableCode().includes(parseInt(req.query.code))){
-        return res.render('join', {error : generate.errorHtml("Code yang anda masukkan salah!") });
+        // return res.render('join', {error : generate.errorHtml("Code yang anda masukkan salah!") });
+        return generate.webPage(res ,'join', {error : generate.errorHtml("Code yang anda masukkan salah!") })
     }
     const code = parseInt(req.query.code);
     const eventIndex = getEvent.index(code);
     const eventName = jsonDB.events[eventIndex].eventName;
-    res.render('feedback', {eventName : eventName });
+    // res.render('feedback', {eventName : eventName });
+    generate.webPage(res ,'feedback', {eventName : eventName})
 });
 
 app.post('/feedback/send', (req, res) => {
     res.redirect('/thanks');
     const code = req.query.code;
-    let name = req.body.name;
+    let name = req.body.name == ""? "Anonim" : req.body.name;
     const eventIndex = getEvent.index(code);
     const feedbackMessage = generate.eventFeedback(name, req.body.feedback);
     jsonDB.events[eventIndex].feedback.push(feedbackMessage)
@@ -147,6 +151,29 @@ const generate = {
     },
     passwordHash: function(pass){
         return bcrypt.hashSync(pass, 10);
+    },
+    webPage: function(res , htmlFile, data){
+        const dataFinal = Object.assign({nav : generate.navHtml}, data) 
+        return res.render(htmlFile, dataFinal)
+    },
+    navHtml: function(){
+        return `<nav>
+        <div id="title"><h2><span class="name-1">Annon</span><span class="name-2">Feed</span></h2></div>
+        <div id="menu" style="display: flex">
+            <div id="sidebar-btn" style="flex-direction:column; align-self: center">
+                <span style="width: 23px; border-bottom: 2px solid white; margin: 3px; display:block"></span>
+                <span style="width: 23px; border-bottom: 2px solid white; margin: 3px; display:block"></span>
+                <span style="width: 23px; border-bottom: 2px solid white; margin: 3px; display:block"></span>
+            </div>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/new">Buat Sesi Baru</a></li>
+                <li><a href="/login">Masuk Kembali</a></li>
+                <li><a href="/join">Bergabung</a></li>
+                <li><a href="/Help">Bantuan</a></li>
+            </ul>
+        </div>
+    </nav>`
     }
 }
 
